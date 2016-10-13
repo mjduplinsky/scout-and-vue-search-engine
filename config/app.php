ae27aae48258954d2692a9b10b212957a816058a
Matthew Duplinsky <matthew@ingenuitydesign.com>
2016-10-13 11:48:32 -0400

    add scout service provider to config/app.php

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //scout service providers
+        Laravel\Scout\ScoutServiceProvider::class,
     ],
 
     /*
